0da7d7d72a2a10bc3c273f8589e2ea881343c22b
Adjoa Darien <adjoa.darien@gmail.com>
2018-04-10 17:30:15 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
+function createPost() {
+  var postContent = document.getElementById("postContent").value;
+  var postAuthor = document.getElementById("postAuthor").value;
 
+  var postTemplate = document.getElementById("post-template").innerHTML;
+
+  //create template function
+  var templateFn = _.template(postTemplate);
+
+  //execute template function with JSON object for the interpolated values
+  var templateHTML = templateFn({content: 'postContent', author: 'postAuthor'});
+
+  var commentsDiv = document.getElementById("comments");
+  //append rather than replace!
+  commentsDiv.innerHTML += templateHTML;
+}
