1c0d305cc6f46e9cd8c049a31bcc94c7cd7278a9
Adjoa Darien <adjoa.darien@gmail.com>
2018-04-10 18:23:44 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -15,3 +15,18 @@ function createPost() {
   var pageDiv = document.getElementById("page");
   pageDiv.innerHTML += pageTemplateHTML;
 }
+
+function postComment() {
+  var commentContent = document.getElementById("commentContent").value;
+  var commentAuthor = document.getElementById("commentAuthor").value;
+
+  var commentTemplate = document.getElementById("comments-template").innerHTML;
+  var commentTemplateFn = _.template(commentTemplate);
+  var commentTemplateHTML = commentTemplateFn({'comment': commentContent, 'commenter': commentAuthor});
+  var commentFromTemplate = commentTemplateHTML.innerHTML;
+
+  var commentsTemplate = document.getElementById("comments-template").innerHTML;
+  var commentsTemplateFn = _.template(commentsTemplate);
+  var commentsTemplateHTML = commentsTemplateFn({'commentBody': commentTemplateHTML})
+  
+}
